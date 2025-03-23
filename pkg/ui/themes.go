fd5568423b11fc6813ca9ae183d68505656ec06f
Christian Uhsat <christian@uhsat.de>
2025-03-23 02:22:08 +0100

    develop

--- pkg/ui/themes.go
+++ pkg/ui/themes.go
@@ -12,22 +12,27 @@ var Themes = map[string][]int32{
     "monokai": {
         0x7f8490, // foreground
         0x222327, // background
-        0xe7c664, // highlight
         0x181819, // info foreground
         0x85d3f2, // info background
         0x181819, // file foreground
         0xa7df78, // file background
         0xe2e2e3, // filter foreground
         0x3e3b48, // filter background
+        0xfc5d7c, // highlight 1
+        0xf39660, // highlight 2
+        0xe7c664, // highlight 3
+        0x9ed072, // highlight 4
+        0x76cce0, // highlight 5
+        0xb39df3, // highlight 6
     },
 }
 
 var (
-    StyleOutput    tcell.Style
-    StyleHighlight tcell.Style
-    StyleInfo      tcell.Style
-    StyleFile      tcell.Style
-    StyleFilter    tcell.Style
+    StyleOutput     tcell.Style
+    StyleInfo       tcell.Style
+    StyleFile       tcell.Style
+    StyleFilter     tcell.Style
+    StyleHighlights []tcell.Style
 )
 
 func setTheme(name string) {
@@ -37,19 +42,23 @@ func setTheme(name string) {
         Foreground(tcell.NewHexColor(t[0])).
         Background(tcell.NewHexColor(t[1]))
 
-    StyleHighlight = tcell.StyleDefault.
-        Foreground(tcell.NewHexColor(t[2])).
-        Background(tcell.NewHexColor(t[1]))
-
     StyleInfo = tcell.StyleDefault.
-        Foreground(tcell.NewHexColor(t[3])).
-        Background(tcell.NewHexColor(t[4]))
+        Foreground(tcell.NewHexColor(t[2])).
+        Background(tcell.NewHexColor(t[3]))
 
     StyleFile = tcell.StyleDefault.
-        Foreground(tcell.NewHexColor(t[5])).
-        Background(tcell.NewHexColor(t[6]))
+        Foreground(tcell.NewHexColor(t[4])).
+        Background(tcell.NewHexColor(t[5]))
 
     StyleFilter = tcell.StyleDefault.
-        Foreground(tcell.NewHexColor(t[7])).
-        Background(tcell.NewHexColor(t[8]))
+        Foreground(tcell.NewHexColor(t[6])).
+        Background(tcell.NewHexColor(t[7]))
+
+    for i := 8; i < 14; i++ {
+        style := tcell.StyleDefault.
+            Foreground(tcell.NewHexColor(t[i])).
+            Background(tcell.NewHexColor(t[1]))
+
+        StyleHighlights = append(StyleHighlights, style)
+    }
 }
