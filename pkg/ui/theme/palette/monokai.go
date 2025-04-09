3ae4b893e287e5058f3a38e82b5f598670120453
Christian Uhsat <christian@uhsat.de>
2025-04-09 18:49:01 +0200

    develop

---
+++ pkg/ui/theme/palette/monokai.go
@@ -0,0 +1,28 @@
+package palette
+
+var Monokai = []int32 {
+    0x7f8490, // output foreground
+    0x222327, // output background
+    0xe2e2e3, // header foreground
+    0x2c2e34, // header background
+    0xe2e2e3, // input foreground
+    0x414550, // input background
+    0x222327, // error foreground
+    0xff6077, // error background
+    0x222327, // mode foreground
+    0xa7df78, // mode background
+    0x595f6f, // hint foreground
+    0x222327, // hint background
+    0x2c2e34, // rule foreground
+    0x222327, // rule background
+    0x222327, // info foreground
+    0x85d3f2, // info background
+    0x595f6f, // line foreground
+    0x2c2e34, // line background
+    0xfc5d7c, // highlight 1
+    0xf39660, // highlight 2
+    0xe7c664, // highlight 3
+    0x9ed072, // highlight 4
+    0x76cce0, // highlight 5
+    0xb39df3, // highlight 6
+}
