9c9ff0274b2526635b9ea0965df2ad19525a119b
Christian Uhsat <christian@uhsat.de>
2025-04-12 00:58:47 +0200

    develop

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -3,6 +3,7 @@ package fs
 import (
     "bufio"
     "fmt"
+    "log"
     "io"
     "os"
 )
@@ -13,14 +14,20 @@ const (
 )
 
 func Debug(a ...any) {
+    log.Println(a...)
+
     fmt.Fprintln(os.Stdout, a...)
 }
 
 func Error(a ...any) {
+    log.Println(a...)
+
     fmt.Fprintln(os.Stderr, a...)
 }
 
 func Panic(a ...any) {
+    log.Println(a...)
+    
     fmt.Fprintln(os.Stderr, a...)
     os.Exit(1)
 }
