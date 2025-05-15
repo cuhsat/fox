7f06e88397ea93fe1f4cbe25abe7d1a69fe5099d
Christian Uhsat <christian@uhsat.de>
2025-05-16 00:32:34 +0200

    develop

--- internal/pkg/user/bag/writer_sql.go
+++ internal/pkg/user/bag/writer_sql.go
@@ -10,54 +10,46 @@ import (
 	_ "embed"
 	_ "modernc.org/sqlite"
 
+	"github.com/cuhsat/fox/api"
 	"github.com/cuhsat/fox/internal/pkg/sys"
 )
 
 var (
-	//go:embed bag.sql
-	schema string
+	// go:embed bag.sql
+	SqlSchema string
 )
-
-type sqlEvidence struct {
-	user   sqlUser
-	file   sqlFile
-	bagged time.Time
-}
-
-type sqlUser struct {
-	login string
-	name  string
-}
-
-type sqlFile struct {
-	path     string
-	hash     string
-	modified time.Time
-	filters  []sqlFilter
-	lines    []sqlLine
-}
-
-type sqlFilter struct {
-	nr    int
-	value string
-}
-
-type sqlLine struct {
-	nr    int
-	value string
-}
 
 type SqlWriter struct {
 	db    *sql.DB      // sql database
-	tx    *sql.Tx      // sql transaction
 	entry *sqlEvidence // current entry
 }
 
-func NewSqlWriter() *SqlWriter {
-	return &SqlWriter{
-		db: nil,
-		tx: nil,
+type sqlEvidence struct {
+	created time.Time
+
+	// user metadata
+	user struct {
+		login string
+		name  string
 	}
+
+	// file metadata
+	file struct {
+		path     string
+		hash     string
+		modified time.Time
+		filters  []sqlData
+		lines    []sqlData
+	}
+}
+
+type sqlData struct {
+	nr    int
+	value string
+}
+
+func NewSqlWriter() *SqlWriter {
+	return new(SqlWriter)
 }
 
 func (w *SqlWriter) Init(f *os.File, n bool, t string) {
@@ -68,15 +60,17 @@ func (w *SqlWriter) Init(f *os.File, n bool, t string) {
 	w.db, err = sql.Open("sqlite", fmt.Sprintf("file:%s", f.Name()))
 
 	if err != nil {
-		sys.Error(err)
+		sys.Panic(err)
 	}
 
-	if n {
-		_, err = w.db.Exec(schema)
+	if !n {
+		return
+	}
 
-		if err != nil {
-			sys.Panic(err)
-		}
+	_, err = w.db.Exec(api.SqlSchema)
+
+	if err != nil {
+		sys.Panic(err)
 	}
 }
 
@@ -85,79 +79,47 @@ func (w *SqlWriter) Start() {
 }
 
 func (w *SqlWriter) Finalize() {
-	var err error
-
-	w.tx, err = w.db.Begin()
+	tx, err := w.db.Begin()
 
 	if err != nil {
 		sys.Error(err)
+		return
 	}
 
-	res, err := w.db.Exec(
-		`INSERT INTO users (login, name) VALUES (?, ?);`,
+	user_id := w.insert(`users (login, name)`,
 		w.entry.user.login,
 		w.entry.user.name,
 	)
 
-	if err != nil {
-		sys.Error(err)
-	}
-
-	user_id, err := res.LastInsertId()
-
-	if err != nil {
-		sys.Error(err)
-	}
-
-	res, err = w.db.Exec(
-		`INSERT INTO files (path, hash, modified) VALUES (?, ?, ?);`,
+	file_id := w.insert(`files (path, hash, modified)`,
 		w.entry.file.path,
 		w.entry.file.hash,
 		w.entry.file.modified,
 	)
 
-	if err != nil {
-		sys.Error(err)
-	}
-
-	file_id, err := res.LastInsertId()
-
-	if err != nil {
-		sys.Error(err)
-	}
-
 	for _, f := range w.entry.file.filters {
-		if _, err := w.db.Exec(
-			`INSERT INTO filters (file_id, nr, value) VALUES (?, ?, ?);`,
+		_ = w.insert(`filters (file_id, nr, value)`,
 			file_id,
 			f.nr,
 			f.value,
-		); err != nil {
-			sys.Error(err)
-		}
+		)
 	}
 
 	for _, l := range w.entry.file.lines {
-		if _, err := w.db.Exec(
-			`INSERT INTO lines (file_id, nr, value) VALUES (?, ?, ?);`,
+		_ = w.insert(`lines (file_id, nr, value)`,
 			file_id,
 			l.nr,
 			l.value,
-		); err != nil {
-			sys.Error(err)
-		}
+		)
 	}
 
-	if _, err := w.db.Exec(
-		`INSERT INTO evidence (user_id, file_id, bagged) VALUES (?, ?, ?);`,
+	_ = w.insert(`evidence (user_id, file_id, created)`,
 		user_id,
 		file_id,
-		w.entry.bagged,
-	); err != nil {
-		sys.Error(err)
-	}
+		w.entry.created,
+	)
 
-	err = w.tx.Commit()
+	err = tx.Commit()
 
 	if err != nil {
 		sys.Error(err)
@@ -165,25 +127,22 @@ func (w *SqlWriter) Finalize() {
 }
 
 func (w *SqlWriter) WriteFile(p string, fs []string) {
-	w.entry.file = sqlFile{
-		path: p,
-	}
+	w.entry.file.path = p
 
 	for i, f := range fs {
-		w.entry.file.filters = append(w.entry.file.filters, sqlFilter{
+		w.entry.file.filters = append(w.entry.file.filters, sqlData{
 			nr: i, value: f,
 		})
 	}
 }
 
 func (w *SqlWriter) WriteUser(u *user.User) {
-	w.entry.user = sqlUser{
-		login: u.Username, name: u.Name,
-	}
+	w.entry.user.login = u.Username
+	w.entry.user.name = u.Name
 }
 
 func (w *SqlWriter) WriteTime(t, f time.Time) {
-	w.entry.bagged = t.UTC()
+	w.entry.created = t.UTC()
 	w.entry.file.modified = f.UTC()
 }
 
@@ -193,8 +152,32 @@ func (w *SqlWriter) WriteHash(b []byte) {
 
 func (w *SqlWriter) WriteLines(ns []int, ss []string) {
 	for i := range ss {
-		w.entry.file.lines = append(w.entry.file.lines, sqlLine{
+		w.entry.file.lines = append(w.entry.file.lines, sqlData{
 			nr: ns[i], value: ss[i],
 		})
 	}
+}
+
+func (w *SqlWriter) insert(q string, v ...any) int64 {
+	f := "?"
+
+	for range len(v) - 1 {
+		f += ", ?"
+	}
+
+	res, err := w.db.Exec(fmt.Sprintf("INSERT INTO %s VALUES (%s);", q, f), v...)
+
+	if err != nil {
+		sys.Error(err)
+		return -1
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		sys.Error(err)
+		return -1
+	}
+
+	return id
 }
