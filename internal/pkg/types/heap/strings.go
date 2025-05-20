8bfdefa4b608edb2ff8e0e5c06ad8784a80f9a90
Christian Uhsat <christian@uhsat.de>
2025-05-20 13:14:47 +0200

    develop

--- internal/pkg/types/heap/strings.go
+++ internal/pkg/types/heap/strings.go
@@ -1,7 +1,8 @@
 package heap
 
 import (
-	"bytes"
+	"unicode"
+	"unicode/utf8"
 )
 
 const (
@@ -16,25 +17,69 @@ const (
 func (h *Heap) Strings() <-chan string {
 	ch := make(chan string)
 
+	rc := make(chan byte, 1024)
+
 	go func() {
-		var b bytes.Buffer
+		for _, c := range *h.mmap {
+			rc <- c
+		}
+
+		close(rc)
+	}()
+
+	go func() {
+		var s []rune
+
+		buf := make([]byte, 4)
 
 		h.RLock()
 
-		for _, c := range *h.mmap {
-			if c >= minASCII && c <= maxASCII {
-				b.WriteByte(c)
-			} else {
-				if b.Len() >= minString {
-					ch <- b.String()
+		for b := range rc {
+			buf[0] = b
+
+			if utf8.RuneStart(b) {
+				w := 1
+				m := utf8ByteCount(b)
+
+				if m > 1 && m <= utf8.UTFMax {
+					for i := 1; i < m; i++ {
+						b, ok := <-rc
+
+						if !ok {
+							break
+						}
+
+						buf[i] = b
+						w++
+					}
 				}
 
-				b.Reset()
+				r, _ := utf8.DecodeRune(buf[:w])
+
+				if r != utf8.RuneError && unicode.IsPrint(r) {
+					s = append(s, r)
+				} else {
+					if len(s) >= minString {
+						ch <- string(s)
+					}
+
+					s = s[:0]
+				}
+			} else {
+				if b >= minASCII && b <= maxASCII {
+					s = append(s, rune(b))
+				} else {
+					if len(buf) >= minString {
+						ch <- string(s)
+					}
+
+					s = s[:0]
+				}
 			}
 		}
 
-		if b.Len() >= minString {
-			ch <- b.String()
+		if len(buf) >= minString {
+			ch <- string(buf)
 		}
 
 		h.RUnlock()
@@ -43,4 +88,18 @@ func (h *Heap) Strings() <-chan string {
 	}()
 
 	return ch
+}
+
+func utf8ByteCount(b byte) int {
+	if b&0x80 == 0 {
+		return 1
+	} else if b&0xE0 == 0xC0 {
+		return 2
+	} else if b&0xF0 == 0xE0 {
+		return 3
+	} else if b&0xF8 == 0xF0 {
+		return 4
+	}
+
+	return 1
 }
