d12d4f8e4d944d8d6e46ca6dd8286d1035357de5
Christian Uhsat <christian.uhsat@codecentric.de>
2025-05-28 14:45:10 +0200

    develop

---
+++ internal/pkg/types/filters.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	"fmt"
+)
+
+type Filters []string
+
+// singleton
+var filters *Filters = nil
+
+func GetFilters() *Filters {
+	if filters == nil {
+		filters = new(Filters)
+	}
+
+	return filters
+}
+
+func (f *Filters) String() string {
+	return fmt.Sprintf("%v", *f)
+}
+
+func (f *Filters) Type() string {
+	return "strings"
+}
+
+// global
+func (f *Filters) Set(p string) error {
+	*f = append(*f, p)
+
+	return nil
+}
+
+// global
+func (f *Filters) Pop() {
+	*f = (*f)[:len(*f)-1]
+}
