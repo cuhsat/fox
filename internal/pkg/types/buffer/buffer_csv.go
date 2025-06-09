ad43b82c80470c8e7fdced59bd6c0280cece587f
Christian Uhsat <christian@uhsat.de>
2025-06-09 23:40:28 +0200

    develop

--- internal/pkg/types/buffer/buffer_csv.go
+++ internal/pkg/types/buffer/buffer_csv.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/cuhsat/fox/internal/pkg/text"
+	"github.com/cuhsat/fox/internal/pkg/types/smap"
 )
 
 type CsvBuffer struct {
@@ -22,19 +23,24 @@ type CsvLine struct {
 }
 
 type CMap struct {
-	Strs []string
-	Rows []int
+	SMap *smap.SMap
 
-	w int
-	h int
+	Lens *[]int
+
+	W int
+	H int
 }
 
 func Csv(ctx *Context) (buf CsvBuffer) {
+	buf.N = text.Dec(ctx.Heap.Count())
+
 	cache, key := ctx.Heap.Cache(), ctx.Hash("csv")
 
 	if val, ok := cache[key]; ok {
 		buf.CMap = val.(*CMap)
 	} else {
+		var sb strings.Builder
+
 		sr := strings.NewReader(ctx.Heap.SMap().String())
 		cr := csv.NewReader(sr)
 
@@ -44,68 +50,65 @@ func Csv(ctx *Context) (buf CsvBuffer) {
 			panic(err) // TODO
 		}
 
-		buf.CMap = &CMap{
-			make([]string, len(cols)),
-			make([]int, len(cols[0])),
-			0,
-			0,
-		}
+		smap := make(smap.SMap, len(cols))
+		lens := make([]int, len(cols[0]))
+
+		buf.CMap = &CMap{&smap, &lens, 0, 0}
 
 		// calculate row max length
 		for _, rows := range cols {
-			for r, row := range rows {
-				buf.CMap.Rows[r] = max(text.Len(row), buf.CMap.Rows[r])
+			for i, row := range rows {
+				lens[i] = max(text.Len(row), lens[i])
 			}
 		}
 
 		// calculate buffer width
-		for _, l := range buf.CMap.Rows {
-			buf.CMap.w += l + 3
+		for _, l := range lens {
+			buf.CMap.W += l + 3
 		}
 
 		// prepadd all rows
-		for c, rows := range cols {
-			var sb strings.Builder
-
-			for r, row := range rows {
-				sb.WriteString(text.Padd(row, buf.CMap.Rows[r]))
+		for i, rows := range cols {
+			for j, row := range rows {
+				sb.WriteString(text.Padd(row, lens[j]))
 				sb.WriteString("   ")
 			}
 
-			buf.CMap.Strs[c] = sb.String()
+			smap[i].Nr = i
+			smap[i].Str = sb.String()
+
+			sb.Reset()
 		}
 
-		buf.CMap.w -= 2
-		buf.CMap.h = len(buf.CMap.Strs)
+		buf.CMap.W -= 2
+		buf.CMap.H = len(smap)
 
 		cache[key] = buf.CMap
 	}
 
-	buf.W = buf.CMap.w
-	buf.H = buf.CMap.h
-
-	buf.N = text.Dec(ctx.Heap.Count())
+	buf.W = buf.CMap.W
+	buf.H = buf.CMap.H
 
 	buf.Lines = make(chan CsvLine, Size)
 
-	csvLine := func(nr int, s string) CsvLine {
+	csvLine := func(str smap.String) CsvLine {
 		return CsvLine{Line{
-			fmt.Sprintf("%0*d", buf.N, nr),
-			text.Trim(s, min(ctx.X, text.Len(s)), ctx.W),
+			fmt.Sprintf("%0*d", buf.N, str.Nr),
+			text.Trim(str.Str, min(ctx.X, text.Len(str.Str)), ctx.W),
 		}}
 	}
 
 	go func() {
 		defer close(buf.Lines)
 
-		buf.Lines <- csvLine(0, buf.CMap.Strs[0])
+		buf.Lines <- csvLine((*buf.CMap.SMap)[0])
 
-		for y, s := range buf.CMap.Strs[ctx.Y+1:] {
+		for y, str := range (*buf.CMap.SMap)[ctx.Y+1:] {
 			if y >= ctx.H-1 {
 				return
 			}
 
-			buf.Lines <- csvLine(ctx.Y+1+y, s)
+			buf.Lines <- csvLine(str)
 		}
 	}()
 
