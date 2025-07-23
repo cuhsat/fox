d11a2a41c6cd02232a47caf6e99183b60dd5f7fc
Christian Uhsat <christian@uhsat.de>
2025-07-23 17:57:52 +0200

    develop

--- internal/fox/ui/ui.go
+++ internal/fox/ui/ui.go
@@ -411,7 +411,7 @@ func (ui *UI) Run(hs *heapset.HeapSet, hi *history.History, bag *bag.Bag) {
 					}
 
 					if bag.Put(heap) {
-						ui.overlay.SendInfo(fmt.Sprintf("%s saved to %s", heap.String(), bag.Path))
+						ui.overlay.SendInfo(fmt.Sprintf("%s saved to %s", heap.String(), bag.String()))
 					}
 
 				case tcell.KeyCtrlB:
