549863f656dbfa743e6269cd21610a00ed498ac0
Christian Uhsat <christian@uhsat.de>
2025-08-30 12:46:07 +0200

    release

---
+++ internal/pkg/sys/fs/forensic_fs.go
@@ -0,0 +1,109 @@
+package fs
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/afero"
+)
+
+type ForensicFs struct {
+	base  afero.Fs
+	layer afero.Fs
+}
+
+func NewForensicFs(base, layer afero.Fs) *ForensicFs {
+	return &ForensicFs{base: base, layer: layer}
+}
+
+func (fs *ForensicFs) Chmod(name string, mode os.FileMode) error {
+	return fs.layer.Chmod(name, mode)
+}
+
+func (fs *ForensicFs) Chown(name string, uid, gid int) error {
+	return fs.layer.Chown(name, uid, gid)
+}
+
+func (fs *ForensicFs) Chtimes(name string, atime time.Time, mtime time.Time) error {
+	return fs.layer.Chtimes(name, atime, mtime)
+}
+
+func (fs *ForensicFs) Create(name string) (afero.File, error) {
+	return fs.layer.Create(name)
+}
+
+func (fs *ForensicFs) Mkdir(name string, perm os.FileMode) error {
+	return fs.layer.Mkdir(name, perm)
+}
+
+func (fs *ForensicFs) MkdirAll(path string, perm os.FileMode) error {
+	return fs.layer.MkdirAll(path, perm)
+}
+
+func (fs *ForensicFs) Name() string {
+	return "ForensicFs"
+}
+
+func (fs *ForensicFs) Open(name string) (afero.File, error) {
+	lf, err := fs.layer.Open(name)
+
+	if err == nil {
+		return lf, nil // layer file
+	}
+
+	bf, err := fs.base.Open(name)
+
+	if err == nil {
+		return bf, nil // base file
+	}
+
+	return nil, err
+}
+
+func (fs *ForensicFs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
+	lf, err := fs.layer.OpenFile(name, flag, perm)
+
+	if err == nil {
+		return lf, nil // layer file
+	}
+
+	bf, err := fs.base.OpenFile(name, flag, perm)
+
+	if err == nil {
+		return bf, nil // base file
+	}
+
+	return nil, err
+}
+
+func (fs *ForensicFs) Remove(name string) error {
+	return fs.layer.Remove(name)
+}
+
+func (fs *ForensicFs) RemoveAll(path string) error {
+	return fs.layer.Remove(path)
+}
+
+func (fs *ForensicFs) Rename(oldname, newname string) error {
+	return fs.layer.Rename(oldname, newname)
+}
+
+func (fs *ForensicFs) Stat(name string) (os.FileInfo, error) {
+	return fs.layer.Stat(name)
+}
+
+func (fs *ForensicFs) Exists(name string) bool {
+	_, err := fs.layer.Stat(name)
+
+	if err == nil {
+		return true
+	}
+
+	_, err = fs.base.Stat(name)
+
+	if err == nil {
+		return true
+	}
+
+	return false
+}
--- internal/pkg/sys/mem.go
+++ internal/pkg/sys/mem.go
@@ -1,59 +1,46 @@
 package sys
 
 import (
-	"os"
-
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/afero"
 
-	"github.com/cuhsat/fox/internal/pkg/sys/fs"
+	foxfs "github.com/cuhsat/fox/internal/pkg/sys/fs"
 )
 
 var Watcher, _ = fsnotify.NewBufferedWatcher(1024)
 
-var mem = fs.NewNotifyFs(afero.NewMemMapFs(), Watcher)
+var ffs = foxfs.NewForensicFs(
+	afero.NewReadOnlyFs(
+		afero.NewOsFs(),
+	),
+	foxfs.NewNotifyFs(
+		afero.NewMemMapFs(),
+		Watcher,
+	),
+)
 
 type File = afero.File
 
 func Open(path string) File {
-	mf, err := mem.Open(path)
+	f, err := ffs.Open(path)
 
-	if err == nil {
-		return mf // memory file
+	if err != nil {
+		Error(err)
 	}
 
-	rf, err := os.OpenFile(path, os.O_RDONLY, 0400)
-
-	if err == nil {
-		return rf // regular file
-	}
-
-	Panic(err)
-	return nil
+	return f
 }
 
 func Create(path string) File {
-	f, err := mem.Create(path)
+	f, err := ffs.Create(path)
 
 	if err != nil {
-		Panic(err)
+		Error(err)
 	}
 
 	return f
 }
 
 func Exists(path string) bool {
-	_, err := mem.Stat(path)
-
-	if err == nil {
-		return true
-	}
-
-	_, err = os.Stat(path)
-
-	if err == nil {
-		return true
-	}
-
-	return false
+	return ffs.Exists(path)
 }
