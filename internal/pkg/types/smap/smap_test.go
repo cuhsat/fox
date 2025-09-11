876bd92817d763283473015cffc2240cbbf6e8fb
Christian Uhsat <christian@uhsat.de>
2025-09-11 19:08:23 +0200

    develop

--- internal/pkg/types/smap/smap_test.go
+++ internal/pkg/types/smap/smap_test.go
@@ -11,33 +11,9 @@ import (
 	"github.com/edsrzf/mmap-go"
 )
 
-func Testdata(name string) (*os.File, *mmap.MMap, error) {
-	_, c, _, ok := runtime.Caller(0)
-
-	if !ok {
-		return nil, nil, errors.New("error")
-	}
-
-	p := filepath.Join(filepath.Dir(c), "..", "..", "..", "..", "testdata", name)
-
-	f, err := os.OpenFile(p, os.O_RDONLY, 0400)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	m, err := mmap.Map(f, mmap.RDONLY, 0)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return f, &m, nil
-}
-
 func BenchmarkMap(b *testing.B) {
 	b.Run("Benchmark Map", func(b *testing.B) {
-		f, m, err := Testdata("test.txt")
+		f, m, err := testdata("test.txt")
 
 		if err != nil {
 			b.Fatal(err)
@@ -61,7 +37,7 @@ func BenchmarkMap(b *testing.B) {
 
 func BenchmarkRender(b *testing.B) {
 	b.Run("Benchmark Render", func(b *testing.B) {
-		f, m, err := Testdata("test.txt")
+		f, m, err := testdata("test.txt")
 
 		if err != nil {
 			b.Fatal(err)
@@ -87,7 +63,7 @@ func BenchmarkRender(b *testing.B) {
 
 func BenchmarkIndent(b *testing.B) {
 	b.Run("Benchmark Indent", func(b *testing.B) {
-		f, m, err := Testdata("test.json")
+		f, m, err := testdata("test.json")
 
 		if err != nil {
 			b.Fatal(err)
@@ -113,7 +89,7 @@ func BenchmarkIndent(b *testing.B) {
 
 func BenchmarkWrap(b *testing.B) {
 	b.Run("Benchmark Wrap", func(b *testing.B) {
-		f, m, err := Testdata("test.txt")
+		f, m, err := testdata("test.txt")
 
 		if err != nil {
 			b.Fatal(err)
@@ -139,7 +115,7 @@ func BenchmarkWrap(b *testing.B) {
 
 func BenchmarkGrep(b *testing.B) {
 	b.Run("Benchmark Grep", func(b *testing.B) {
-		f, m, err := Testdata("test.txt")
+		f, m, err := testdata("test.txt")
 
 		if err != nil {
 			b.Fatal(err)
@@ -154,12 +130,139 @@ func BenchmarkGrep(b *testing.B) {
 		}(m)
 
 		s := Map(m)
-		r := regexp.MustCompile(".*")
+
+		re := regexp.MustCompile(".*")
 
 		b.ResetTimer()
 
 		for b.Loop() {
-			s.Grep(r)
+			s.Grep(re)
 		}
 	})
+}
+
+func TestMap(t *testing.T) {
+	t.Run("Test Grep", func(t *testing.T) {
+		f, m, err := testdata("test.txt")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w, h := Map(m).Size()
+
+		_ = m.Unmap()
+		_ = f.Close()
+
+		if w != 545 || h != 31107 {
+			t.Fatal("wrong size")
+		}
+	})
+}
+
+func TestRender(t *testing.T) {
+	t.Run("Test Render", func(t *testing.T) {
+		b := []byte("\ttest\n")
+		v := "    test\n"
+
+		s := Map((*mmap.MMap)(&b)).Render()
+
+		w, h := s.Size()
+
+		if w != 8 || h != 1 {
+			t.Fatal("wrong length")
+		}
+
+		if s.String() != v {
+			t.Fatal("wrong string")
+		}
+	})
+}
+
+func TestIndent(t *testing.T) {
+	t.Run("Test Indent", func(t *testing.T) {
+		b := []byte(`[{"test":123}]`)
+		v := "[\n  {\n    \"test\": 123\n  }\n]\n"
+
+		s := Map((*mmap.MMap)(&b)).Indent()
+
+		w, h := s.Size()
+
+		if w != 15 || h != 5 {
+			t.Fatal("wrong length")
+		}
+
+		if s.String() != v {
+			t.Fatal("wrong string")
+		}
+	})
+}
+
+func TestWrap(t *testing.T) {
+	t.Run("Test Wrap", func(t *testing.T) {
+		b := []byte(`testtest`)
+		v := "test\ntest\n"
+
+		s := Map((*mmap.MMap)(&b)).Wrap(4)
+
+		w, h := s.Size()
+
+		if w != 4 || h != 2 {
+			t.Fatal("wrong length")
+		}
+
+		if s.String() != v {
+			t.Fatal("wrong string")
+		}
+	})
+}
+
+func TestGrep(t *testing.T) {
+	t.Run("Test Map", func(t *testing.T) {
+		f, m, err := testdata("test.ioc")
+		v := "test@example.org\nhttps://example.org\n"
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		re := regexp.MustCompile("example")
+
+		s := Map(m).Grep(re)
+
+		_ = m.Unmap()
+		_ = f.Close()
+
+		if len(*s) != 2 {
+			t.Fatal("wrong length")
+		}
+
+		if s.String() != v {
+			t.Fatal("wrong string")
+		}
+	})
+}
+
+func testdata(name string) (*os.File, *mmap.MMap, error) {
+	_, c, _, ok := runtime.Caller(0)
+
+	if !ok {
+		return nil, nil, errors.New("error")
+	}
+
+	p := filepath.Join(filepath.Dir(c), "..", "..", "..", "..", "testdata", name)
+
+	f, err := os.OpenFile(p, os.O_RDONLY, 0400)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	m, err := mmap.Map(f, mmap.RDONLY, 0)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return f, &m, nil
 }
